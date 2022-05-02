7ec29a1c8c08170cad6ef7a659a1c14a7b46a341
Mark <mgalex12@icloud.com>
2022-05-02 15:33:15 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+function superbowlWin(arrayOfSb) {
+    let found = arrayOfSb.find((element) => element.result === "W");
+    console.log(found)
+    if(found){
+    return(found.year)
+}
+    console.log(found)
+}
+
+
